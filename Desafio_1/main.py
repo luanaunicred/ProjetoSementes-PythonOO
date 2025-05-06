27a34d636ec14a0dae7f5dc67b0a5b9cb12e8f04
luana.souza <luana.souza@e-unicred.com.br>
2025-05-06 16:35:43 -0300

    Ajuste para leitura dos dados por arquivo json.

--- Desafio_1/main.py
+++ Desafio_1/main.py
@@ -1,32 +1,21 @@
-from mercado import (Mercado)
-from mercado import (Produto)
+import json
+from mercado import Mercado, Produto
+
 
 mercado = Mercado()
 
-p1 = Produto(1,'Arroz', 50, 20.99)                      #1.049,50
-p2 = Produto(2, 'Feijão', 30, 8.50)                     #255,00
-p3 = Produto(3,'Macarrão', 40, 5.90)                    #236,00
-p4 = Produto(4,'Açucar', 25, 4.20)                      #105,00
-p5 = Produto(5, 'Café', 60, 4.20)                       #252,00
-p6 = Produto(6, 'Leite', 100, 4.80)                     #480,00
-p7 = Produto(7, 'Óleo de Soja', 70, 9.50)               #665,00
-p8 = Produto(8, 'Sabonete', 200, 1.50)                  #300,00
-p9 = Produto(9, 'Detergente', 150, 2.3)                 #345,00
-p10 = Produto(10, 'Papel Higiênico', 80, 12.00)         #960,00
+with open('produtos.json.py', 'r', encoding='utf-8') as file:
+    lista_produtos = json.load(file)
 
-mercado.inserir_produto(p1)
-mercado.inserir_produto(p2)
-mercado.inserir_produto(p3)
-mercado.inserir_produto(p4)
-mercado.inserir_produto(p5)
-mercado.inserir_produto(p6)
-mercado.inserir_produto(p7)
-mercado.inserir_produto(p8)
-mercado.inserir_produto(p9)
-mercado.inserir_produto(p10)
+for item in lista_produtos:
+    produto = Produto(
+        id=item["id"],
+        nome=item["nome"],
+        quantidade=item["quantidade"],
+        valor=item["valor"]
+    )
+    mercado.inserir_produto(produto)
 
 mercado.lista_produto()
-print(mercado.lista_produto())
 
-mercado.calcular_valor_total_estoque()
-print(mercado.calcular_valor_total_estoque())
+print(f"\nValor total do estoque: R${mercado.calcular_valor_total_estoque():.2f}")
---
+++ Desafio_1/produtos.json.py
@@ -0,0 +1,12 @@
+[
+  {"id": 1, "nome": "Arroz", "quantidade": 50, "valor": 20.99},
+  {"id": 2, "nome": "Feijão", "quantidade": 30, "valor": 8.50},
+  {"id": 3, "nome": "Macarrão", "quantidade": 40, "valor": 5.90},
+  {"id": 4, "nome": "Açucar", "quantidade": 25, "valor": 4.20},
+  {"id": 5, "nome": "Café", "quantidade": 60, "valor": 4.20},
+  {"id": 6, "nome": "Leite", "quantidade": 100, "valor": 4.80},
+  {"id": 7, "nome": "Óleo de Soja", "quantidade": 70, "valor": 9.50},
+  {"id": 8, "nome": "Sabonete", "quantidade": 200, "valor": 1.50},
+  {"id": 9, "nome": "Detergente", "quantidade": 150, "valor": 2.30},
+  {"id": 10, "nome": "Papel Higiênico", "quantidade": 80, "valor": 12.00}
+]
